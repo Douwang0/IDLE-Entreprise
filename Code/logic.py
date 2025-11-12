fb9be5e796f6f6d7bdabaddecd139f5e601ff802
Douwang <giulian.lapin@gmail.com>
2025-11-12 09:55:44 +0100

    Logic update

---
+++ Code/logic.py
@@ -0,0 +1,31 @@
+from player import Player
+from elements import *
+from upgrades import *
+
+class Game:
+    def __init__(self):
+        self.tick = 0
+        self.day = 0
+        self.daylenth
+        self.player = Player()
+        self.kayou = Kayou(self.player,0.1)
+        self.elements = {
+        }
+        self.upgrades = {
+        }
+        self.share = {
+        }
+    def update(self):
+        self.tick += 1
+        if self.tick >= self.daylenth:
+            self.tick = 0
+            self.day += 1
+            # appeler game pause interface
+        for item in self.elements.items() + self.upgrades.items() + self.share.items() + [self.kayou]:
+            item.update()
+        # End
+        if self.player.mget() < 0:
+            # appeller game end interface
+            pass
+    def buy(self,id,type):
+        pass
