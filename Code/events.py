6004870f4dd6cb441cf281cfa05cba188f5dd1bd
Khastor <lylianbrias1@gmail.com>
2026-01-07 09:05:34 +0100

    pu

--- Code/events.py
+++ Code/events.py
@@ -62,47 +62,48 @@ class Events:
         elif "gainsmodif" in self.enventtype:
             game.player.bonusmodif(-(self.temp / 100))
         
-        
-        
-        
-        
-    
-def active_event(event:Events, game_ref:Game, gui_ref): #app enverra la référence à game
-    event.action(game_ref)
-    gui_ref.request_event_animation(event.description, 20.0)
-    event.cancer(game_ref)
+class EventManager:
+    def __init__(self,game):
+        self.game = game
+        self.event = Events()
+        self.dic_event = {1 : Events(1, True, 9900, 0, "La lune est percutée par un violent astéroïde, pluie de débris sur Terre : Les kayoux voient leur prix multiplié par 100", ["kayou+"]),
+                    2 : Events(2, True, 3, 180, "Cthulhu fait son retour : La nuit tombe durant 3 minutes", ["timodif"]),
+                    3 : Events(3, True, -20 ,300, "Macron fait son vingtième mandat : Le taux inflation augmente de 20 % pendant 5 jours", ["pricemodif"]), 
+                    4 : Events(4, True, 10, 0, "Sébastien Lecornu est promu premier ministre pour la (nb)ième fois : les employés sont plus efficaces de 10 %", ["employrendmodif"]),
+                    5 : Events(5, True, [0, -50], 60, "Les cavaliers de la mort marchent sur Paris : La demande sur le marché diminue de 50 % pour le jour", ["demmandemodif"]),
+                    6 : Events(6, True, 200, 0, "Pastek, le roi des 11 ciels, donne sa faveur à l'entreprise (nm du joueur): gain instantané de 200 pourcents des gains touchés le jour précédent", ["gainjourinstant"]),
+                    7 : Events(7, True, [0.1] , 60, "Une ampoule a grillé chez l'URSAF: payez 10 pourcents d'impôts en plus à la fin de la journée", ["taxmodif"]),
+                    8 : Events(8, True, 100, 0, "Restock mondial de ciao Kombucha : les employers travaillent deux fois plus efficacement", ["employrendmodif"]),
+                    9 : Events(9, True, [20, 10, 9], 0, "Jesus resucite : Vous gagnez 20 % plus d'argent", ["gainsmodif", "newevent", "cancevent"]),
+                    10 : Events(10, False, [-20, 9, 10], 0, "Jésus est crucifié de nouveau : Vous perdez votre bonus de 20 pourcents de gains", ["gainsmodif","newevent","cancevent"]),
+                    11 : Events(11, True, [-1, -100], 60, "Le Ragnarok débute : vous n'avez plus d'impôts ni de demande sur le marché pour ce jour", ["demmandemodif", "taxmodif"]),
+                    12 : Events(12, True, [0, 0, 12], 0, "Les aliens débarquent : baisse du prix des employés", ["aliens", "cancevent"]),
+                    13 : Events(13, True, 0, 0, "Il est lundi (ou pas jsp en vrai)", []),
+                    14 : Events(14, True, [0, 15, 14], 0, "Vous découvrez que tout l'argent du monde vous a été promis il y a 3000 ans : Jérusalem est découverte", ["newevent", "cancevent"]),
+                    15 : Events(15, False, [0, 16, 15], 0, "Annexion de Jérusalem : Vous pouvez vous évader fiscalement à Jérusalem", ["newevent", "cancevent"]),
+                    16 : Events(16, False, [0], 0, "Evasion fiscale à Jérusalem : ne payez plus d'impôts", ["taxmodif"]),
+                    17 : Events(17, True, 0, 0, "Les gilets jaunes font leur retours : le prix de l'essence augmente ", []),
+                    18 : Events(18, True, [-0.1, 19, 18], 0, "Votre femme vous a trompé, elle prend les enfants et 50 pourcents de vos biens, vous devez maintenant payer la pension alimentaire jusqu'à votre mort.", ["pertemoitierinstant", "gainsmodif", "newevent", "cancevent"]),
+                    19 : Events(19, False, [0, 20, 21], 0, "Vous découvrez la théorie de l'internet mort ", ["newevent", "cancevent"]),
+                    20 : Events(20, False, [0, 21, 20], 0, "Vous publiez un article sur cette théorie et devenez connus", ["newevent", "cancevent"]),
+                    21 : Events(21, False, [0.1, 22, 21], 0, "Votre femme revient", ["gainsmodif", "newevent", "cancevent"]),
+                    22 : Events(22, False, [23, 22], 0, "Vous créez une IA pour mettre de la vie sur internet", ["newevent", "cancevent"]),
+                    23 : Events(23, False, 13, 0, "l'IA contrôle internet : +13 pourcents de parts de marchées ", ["gainparts"]),
+                    24 : Events(24, True, 0, 0, "Vous êtes de bonne humeur aujourd'hui", []),
+                    25 : Events(25, True, 0, 0, "Contrôle fiscal (-50 pourcents d'argent)", ["pertemoitierinstant"]),
+                    26 : Events(26, True, -7, 180, "La machine a boisson du groupe << ice team >> a cessé de fonctionné, baisse de production des empoyés de 7% pendant 3 jours", ["employrendmodif"]),
+                    27 : Events(27, True, -10, 60, "Vous avez trop dormi, les employés on eu la flemme de travailler", ["employrendmodif"]),
+                    28 : Events(28, True, 0, 0, "Il pleut", []),
+                    29 : Events(29, True, 0, 0, "Vous êtes sur la liste d'Epstein", []),
+                    30 : Events(30, True, 50, 180, "Livraison d'Ice Tea, 50 pourcents d'efficacité des employés en plus", ["employrendmodif"]),
+                    31 : Events(31, True, 0, 0, "Votre fils est con", []),
+                    32 : Events(32, True, 200, 0, "Vous passez par la case départ", ["gaininstant"]),
+                    33 : Events(33, True, 2.5, 0, "Vous volez la carte vitale d'un employé", ["gaininstant"]),
+                    34 : Events(34, True, 0, 0, "Un employé à lâché une caisse, il faut la réparer (-50 pourcents d'argent)", ["pertemoitierinstant"])}
 
-dic_event = {1 : Events(1, True, 9900, 0, "La lune est percutée par un violent astéroïde, pluie de débris sur Terre : Les kayoux voient leur prix multiplié par 100", ["kayou+"]),
-             2 : Events(2, True, 3, 180, "Cthulhu fait son retour : La nuit tombe durant 3 minutes", ["timodif"]),
-             3 : Events(3, True, -20 ,300, "Macron fait son vingtième mandat : Le taux inflation augmente de 20 % pendant 5 jours", ["pricemodif"]), 
-             4 : Events(4, True, 10, 0, "Sébastien Lecornu est promu premier ministre pour la (nb)ième fois : les employés sont plus efficaces de 10 %", ["employrendmodif"]),
-             5 : Events(5, True, [0, -50], 60, "Les cavaliers de la mort marchent sur Paris : La demande sur le marché diminue de 50 % pour le jour", ["demmandemodif"]),
-             6 : Events(6, True, 200, 0, "Pastek, le roi des 11 ciels, donne sa faveur à l'entreprise (nm du joueur): gain instantané de 200 pourcents des gains touchés le jour précédent", ["gainjourinstant"]),
-             7 : Events(7, True, [0.1] , 60, "Une ampoule a grillé chez l'URSAF: payez 10 pourcents d'impôts en plus à la fin de la journée", ["taxmodif"]),
-             8 : Events(8, True, 100, 0, "Restock mondial de ciao Kombucha : les employers travaillent deux fois plus efficacement", ["employrendmodif"]),
-             9 : Events(9, True, [20, 10, 9], 0, "Jesus resucite : Vous gagnez 20 % plus d'argent", ["gainsmodif", "newevent", "cancevent"]),
-             10 : Events(10, False, [-20, 9, 10], 0, "Jésus est crucifié de nouveau : Vous perdez votre bonus de 20 pourcents de gains", ["gainsmodif","newevent","cancevent"]),
-             11 : Events(11, True, [-1, -100], 60, "Le Ragnarok débute : vous n'avez plus d'impôts ni de demande sur le marché pour ce jour", ["demmandemodif", "taxmodif"]),
-             12 : Events(12, True, [0, 0, 12], 0, "Les aliens débarquent : baisse du prix des employés", ["aliens", "cancevent"]),
-             13 : Events(13, True, 0, 0, "Il est lundi (ou pas jsp en vrai)", []),
-             14 : Events(14, True, [0, 15, 14], 0, "Vous découvrez que tout l'argent du monde vous a été promis il y a 3000 ans : Jérusalem est découverte", ["newevent", "cancevent"]),
-             15 : Events(15, False, [0, 16, 15], 0, "Annexion de Jérusalem : Vous pouvez vous évader fiscalement à Jérusalem", ["newevent", "cancevent"]),
-             16 : Events(16, False, [0], 0, "Evasion fiscale à Jérusalem : ne payez plus d'impôts", ["taxmodif"]),
-             17 : Events(17, True, 0, 0, "Les gilets jaunes font leur retours : le prix de l'essence augmente ", []),
-             18 : Events(18, True, [-0.1, 19, 18], 0, "Votre femme vous a trompé, elle prend les enfants et 50 pourcents de vos biens, vous devez maintenant payer la pension alimentaire jusqu'à votre mort.", ["pertemoitierinstant", "gainsmodif", "newevent", "cancevent"]),
-             19 : Events(19, False, [0, 20, 21], 0, "Vous découvrez la théorie de l'internet mort ", ["newevent", "cancevent"]),
-             20 : Events(20, False, [0, 21, 20], 0, "Vous publiez un article sur cette théorie et devenez connus", ["newevent", "cancevent"]),
-             21 : Events(21, False, [0.1, 22, 21], 0, "Votre femme revient", ["gainsmodif", "newevent", "cancevent"]),
-             22 : Events(22, False, [23, 22], 0, "Vous créez une IA pour mettre de la vie sur internet", ["newevent", "cancevent"]),
-             23 : Events(23, False, 13, 0, "l'IA contrôle internet : +13 pourcents de parts de marchées ", ["gainparts"]),
-             24 : Events(24, True, 0, 0, "Vous êtes de bonne humeur aujourd'hui", []),
-             25 : Events(25, True, 0, 0, "Contrôle fiscal (-50 pourcents d'argent)", ["pertemoitierinstant"]),
-             26 : Events(26, True, -7, 180, "La machine a boisson du groupe << ice team >> a cessé de fonctionné, baisse de production des empoyés de 7% pendant 3 jours", ["employrendmodif"]),
-             27 : Events(27, True, -10, 60, "Vous avez trop dormi, les employés on eu la flemme de travailler", ["employrendmodif"]),
-             28 : Events(28, True, 0, 0, "Il pleut", []),
-             29 : Events(29, True, 0, 0, "Vous êtes sur la liste d'Epstein", []),
-             30 : Events(30, True, 50, 180, "Livraison d'Ice Tea, 50 pourcents d'efficacité des employés en plus", ["employrendmodif"]),
-             31 : Events(31, True, 0, 0, "Votre fils est con", []),
-             32 : Events(32, True, 200, 0, "Vous passez par la case départ", ["gaininstant"]),
-             33 : Events(33, True, 2.5, 0, "Vous volez la carte vitale d'un employé", ["gaininstant"]),
-             34 : Events(34, True, 0, 0, "Un employé à lâché une caisse, il faut la réparer (-50 pourcents d'argent)", ["pertemoitierinstant"])}
+    def active_event(self,event:Events, gui_ref): #app enverra la référence à game
+        event.action(self.game)
+        gui_ref.request_event_animation(event.description, 20.0)
+        event.cancer(game_ref)
+    def update(self):
+        pass
